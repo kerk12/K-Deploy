357b1db99731e5f42b51388032c0e5804af6de10
Kyriakos Giannakis <kerk12gr@gmail.com>
2020-07-14 12:53:52 +0300

    Added up and down. Refactoring.

--- k-deploy.py
+++ k-deploy.py
@@ -18,39 +18,63 @@ Developed by Kyriakos Giannakis (kgiannakis@kgiannakis.me). Blame him should any
 
 """)
 
+def stop_container(kd, name, raise_exc=True):
+    running_containers = kd.get_running_containers_by_name()
+    if name not in running_containers:
+        if not raise_exc:
+            return
+        raise ValueError("Container '{}' not running.".format(name))
+    print("Stopping old container.")
+    killed = kd.check_and_stop(running_containers[name])
+    print("Killed container after not responding." if killed else "Container stopped.")
+
+def rm_container(kd, name):
+    all_containers = kd.get_all_containers_by_name()
+    if name in all_containers:
+        c_to_rm = all_containers[name]
+        c_to_rm.remove()
+        print("Old container removed.")
+
+def pull_image(kd, image):
+    print("Pulling {}".format(image))
+    try:
+        kd.pull_image(image)
+    except docker.errors.ImageNotFound:
+        eprint("Error: Image not found. Either the image doesn't exist or you need to 'docker login' first.")
+        exit(1)
+
 client = docker.from_env()
 kd = KDeploy(client)
-print("Pulling {}".format(parser.image))
-try:
-    kd.pull_image(parser.image)
-except docker.errors.ImageNotFound:
-    eprint("Error: Image not found. Either the image doesn't exist or you need to 'docker login' first.")
-    exit(1)
 
+if parser.mode not in ['up', 'down']:
+    raise ValueError("Invalid mode. Valid options are 'up', 'down'.")
+
+name = parser.name.lstrip()
 running_containers = kd.get_running_containers_by_name()
 
-if parser.name in running_containers:
-    print("Stopping old container.")
-    killed = kd.check_and_stop(running_containers[parser.name])
-    if killed: 
-        print("Killed container after not responding.")
 
-all_containers = kd.get_all_containers_by_name()
-if parser.name in all_containers:
-    c_to_rm = all_containers[parser.name]
-    c_to_rm.remove()
-    print("Old container removed.")
+if parser.mode == "up":
+    if not parser.image or not parser.name:
+        raise ValueError("Please give a valid image and container name.")
+    image = parser.image.lstrip()
 
-print("Creating new container...")
-extra_args = {}
-if parser.volumes:
-    extra_args["volumes"] = parser.volumes.split(" ")[1:]
-if parser.network:
-    extra_args["network"] = parser.network.lstrip()
-if parser.ports:
-    extra_args["ports"] = parser.ports.split(" ")[1:]
+    pull_image(kd, image)
+    stop_container(kd, name, False)
+    rm_container(kd, name)
 
-c_new = kd.create(parser.image, parser.name, \
-                **extra_args)
-print("Container created successfully: ")
-print(c_new.id)
+    print("Creating new container...")
+    extra_args = {}
+    if parser.volumes:
+        extra_args["volumes"] = parser.volumes.split(" ")[1:]
+    if parser.network:
+        extra_args["network"] = parser.network.lstrip()
+    if parser.ports:
+        extra_args["ports"] = parser.ports.split(" ")[1:]
+
+    c_new = kd.create(image, name, \
+                    **extra_args)
+    print("Container created successfully: ")
+    print(c_new.id)
+else:
+    stop_container(kd, name)
+    rm_container(kd, name)
--- utils/kparser.py
+++ utils/kparser.py
@@ -3,8 +3,12 @@ import argparse
 _parser = argparse.ArgumentParser(description=\
     "Kuickly Deploy single container docker applications.")
 
-_parser.add_argument('image', type=str, \
+_parser.add_argument('mode', type=str, \
+    help="Whether to spin up or spin down the container. Valid arguments are 'up' and 'down'")
+
+_parser.add_argument('image', type=str, nargs="?", \
     help="The image to be deployed.")
+
 _parser.add_argument('name', type=str, \
     help="The final container's name. Take care when updating a deployed container that you provide the same name otherwise the old container will not be spinned down.")
 _parser.add_argument('-v', '--volumes', type=str, \
