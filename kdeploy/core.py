26b53031d0c60643af95140188341ec4bbdae1db
Kyriakos Giannakis <kerk12gr@gmail.com>
2020-10-17 17:30:57 +0300

    Added forgotten docs to core.py.

--- kdeploy/core.py
+++ kdeploy/core.py
@@ -8,6 +8,9 @@ class KDeploy:
         self.client = docker_client
 
     def get_images_by_name(self):
+        """
+        Get the images of the local docker instance in a form of "tag = img_obj"
+        """
         im_out = {}
         for im in self.client.images.list():
             try:
@@ -17,6 +20,9 @@ class KDeploy:
         return im_out
 
     def get_images_by_digest(self):
+        """
+        Get the images of the local docker instance by their digest in the form of "digest = img_obj".
+        """
         im_out = {}
         for im in self.client.images.list():
             try:
@@ -26,6 +32,11 @@ class KDeploy:
         return im_out
 
     def is_image_present(self, name):
+        """
+        Search if an image is present within the local docker image store.
+
+        Returns the image if found, None if not found.
+        """
         images = self.get_images_by_name()
         for k, im in images.items():
             if name in im.tags:
@@ -33,14 +44,23 @@ class KDeploy:
         return None
 
     def pull_image(self, image):
+        """
+        Pulls an image from an online repo.
+        """
         img, tag = split_image(image)
         self.client.images.pull(img, tag)
 
     def get_running_containers_by_name(self):
+        """
+        Get a map of all the running containers in the form of container_name = container_object.
+        """
         containers = self.client.containers.list()
         return self._map_containers(containers)
 
     def get_all_containers_by_name(self):
+        """
+        Get a map of all the containers (even stopped ones) in the form of container_name = container_object.
+        """
         containers = self.client.containers.list(all=True)
         return self._map_containers(containers)
 
@@ -51,9 +71,17 @@ class KDeploy:
         return cmap
 
     def send_stop_signal(self, container):
+        """
+        Sends SIGTERM to a given container.
+        """
         container.kill(signal="SIGTERM")
 
     def check_and_stop(self, container):
+        """
+        Tries to stop a container gracefully by sending a SIGTERM signal. 
+        Then sleeps for 2 secs and checks if the container is still alive.
+        If the container is still alive after 5 tries, it kills the container with a SIGKILL.
+        """
         self.send_stop_signal(container)
         tries = 1
         while tries <= 5:
@@ -75,6 +103,9 @@ class KDeploy:
         container.stop()
 
     def create(self, image, name, *args, **kwargs):
+        """
+        Create a container.
+        """
         if "network" in kwargs:
             if kwargs["network"] not in self.get_networks_by_name():
                 raise ValueError(
